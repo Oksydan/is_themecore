0d7c9aa9bb094b5d42e13d711cc031617bc6eb0f
Oksydan <oksydan56@gmail.com>
2022-03-23 16:13:19 +0100

    breadcrumbs

---
+++ src/Core/Breadcrumbs/ThemeBreadcrumbs.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Oksydan\Module\IsThemeCore\Core\Breadcrumbs;
+
+class ThemeBreadcrumbs
+{
+    protected $breadcrumbs = [];
+    public $pageName;
+    public $translator;
+
+    function __construct() {
+        $this->context = \Context::getContext();
+        $this->pageName = $this->context->controller->getPageName();
+        $this->getAvailableBreadcrumbs();
+    }
+
+    protected function getAvailableBreadcrumbs()
+    {
+        $this->breadcrumbs = $this->getCommonBreadcrumbs();
+    }
+
+    public function getBreadcrumb()
+    {
+        $breadcrumb = [];
+        $breadcrumb['links'] = $this->getBreadcrumbByPageName();
+        $breadcrumb['count'] = count($breadcrumb['links']);
+
+        return $breadcrumb;
+    }
+
+    public function getBreadcrumbByPageName()
+    {
+        $breadcrumb = [];
+
+        if (isset($this->breadcrumbs[$this->pageName])) {
+            $breadcrumb = $this->breadcrumbs[$this->pageName];
+        }
+
+        return $breadcrumb;
+    }
+
+    protected function getCommonBreadcrumbs()
+    {
+        $pages = [
+            [
+                'controller' => 'cart',
+                'name' => $this->context->getTranslator()->trans('Shopping Cart', array(), 'Shop.Theme.Checkout'),
+            ],
+            [
+                'controller' => 'pagenotfound',
+                'name' => $this->context->getTranslator()->trans('404', array(), 'Shop.Theme.Global'),
+            ],
+            [
+                'controller' => 'stores',
+                'name' => $this->context->getTranslator()->trans('Our stores', array(), 'Shop.Theme.Global'),
+            ],
+            [
+                'controller' => 'sitemap',
+                'name' => $this->context->getTranslator()->trans('Sitemap', array(), 'Shop.Theme.Global'),
+            ],
+        ];
+
+        $breadcrumbs = [];
+        foreach ($pages as $page) {
+            $breadcrumbs[$page['controller']] = [
+                [
+                    'url' => $this->context->link->getPageLink('index'),
+                    'title' => $this->context->getTranslator()->trans('Home', array(), 'Shop.Theme.Global')
+                ],
+                [
+                    'url' => $this->context->link->getPageLink($page['controller']),
+                    'title' => $page['name']
+                ]
+          ];
+        }
+
+        return $breadcrumbs;
+    }
+}
