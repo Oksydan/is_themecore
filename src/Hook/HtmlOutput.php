1e9dae607a762c371ed059b8bb4422f67f1d4e0c
Igor Stępień <igor@istepien.dev>
2022-10-10 22:57:29 +0200

    license fix

--- src/Hook/HtmlOutput.php
+++ src/Hook/HtmlOutput.php
@@ -1,28 +1,7 @@
 <?php
-/**
- * Copyright 2021-2022 InPost S.A.
- *
- * NOTICE OF LICENSE
- *
- * Licensed under the EUPL-1.2 or later.
- * You may not use this work except in compliance with the Licence.
- *
- * You may obtain a copy of the Licence at:
- * https://joinup.ec.europa.eu/software/page/eupl
- * It is also bundled with this package in the file LICENSE.txt
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the Licence is distributed on an AS IS basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions
- * and limitations under the Licence.
- *
- * @author    InPost S.A.
- * @copyright 2021-2022 InPost S.A.
- * @license   https://joinup.ec.europa.eu/software/page/eupl
- */
 
 namespace Oksydan\Module\IsThemeCore\Hook;
+
 use Oksydan\Module\IsThemeCore\Form\Settings\GeneralConfiguration;
 use Oksydan\Module\IsThemeCore\Form\Settings\WebpConfiguration;
 
